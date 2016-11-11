5ad558a9792d6f51907722c5a18f017200a3440e
Miller <sm67c@rc38xcs213.managed.mst.edu>
2016-11-10 22:42:36 -0600

    Fixing omissions from previous commit

---
+++ games/chess/globals.cpp
@@ -0,0 +1,67 @@
+/**************************************************************
+* globals.cpp
+* Definitions for the chess global variables
+* CS5400, FS 2016
+* Stuart Miller
+**************************************************************/
+
+
+/******************************************************
+* Includes
+******************************************************/
+#include "globals.h"
+#include <map>
+#include <iostream>
+
+/******************************************************
+* Local Variables
+******************************************************/
+// Local defs of global variables
+int kingVal;
+int queenVal;
+int rookVal;
+int bishopVal;
+int knightVal;
+int pawnVal;
+int pawnPenalty;
+int mobPenalty;
+int	movesThreshold;
+int	movesEstimate;
+int	maxDepth;
+
+// Definition map
+static std::map<std::string, int*> valConvert = {
+		{ "kingval",		&kingVal },
+		{ "queenval",		&queenVal },
+		{ "rookval",		&rookVal },
+		{ "knightval",		&knightVal },
+		{ "bishopval",		&bishopVal },
+		{ "pawnval",		&pawnVal },
+		{ "pawnpenalty",	&pawnPenalty },
+		{ "mobpenalty",		&mobPenalty },
+		{ "movesthreshold",	&movesThreshold },
+		{ "movesestimate",	&movesEstimate },
+		{ "maxdepth",		&maxDepth }
+	};
+
+/**************************************************************
+* Set Global
+* Sets a global variable according to the passed strings
+**************************************************************/
+void setGlobal( std::string name, std::string value )
+	{
+	if( valConvert.find( name ) != valConvert.end() )
+		{
+		int* numericVal = valConvert[ name ];
+		size_t valueSize = value.size();
+		std::cout << "  Assigning " << value << " to " << name << std::endl;
+		*numericVal = std::stoi( value, &valueSize );
+		}
+
+	// Name not recognized
+	else
+		{
+		std::cout << "  Parameter " << name << " not found" << std::endl;
+		}
+	return;
+	}
---
+++ games/chess/globals.h
@@ -0,0 +1,37 @@
+/**************************************************************
+* globals.h
+* Declarations for the chess global variables
+* CS5400, FS 2016
+* Stuart Miller
+**************************************************************/
+#ifndef JOUEUR_CHESS_GLOBALS_H
+#define JOUEUR_CHESS_GLOBALS_H
+
+/******************************************************
+* Includes
+******************************************************/
+#include <string>
+
+/******************************************************
+* Global Variables
+******************************************************/
+extern int kingVal;
+extern int queenVal;
+extern int rookVal;
+extern int bishopVal;
+extern int knightVal;
+extern int pawnVal;
+extern int pawnPenalty;
+extern int mobPenalty;
+extern int movesThreshold;
+extern int movesEstimate;
+extern int maxDepth;
+
+
+/******************************************************
+* Function Declarations
+******************************************************/
+void setGlobal( std::string name, std::string value );
+
+
+#endif
